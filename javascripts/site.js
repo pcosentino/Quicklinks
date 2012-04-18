cec096d16a9c4210d6622f7a32aae512c8042017
Paolo Cosentino <cosepaol88@gmail.com>
2012-04-18 15:31:08 -0500

    Javascript Files

---
+++ javascripts/site.js
@@ -0,0 +1,37 @@
+
+$(document).ready(function(){
+  
+  
+$(".worklinks").click(function(){
+    $(".panel").slideDown(500).delay(3500).slideUp(500);
+  });
+  
+  
+$(".personallinks").click(function(){
+    $(".panel2").slideDown(500).delay(3500).slideUp(500);
+  });  
+  
+  
+$(".lrs").click(function(){
+    $(".panel3").slideDown(500).delay(3500).slideUp(500);
+  }); 
+
+$(".tds").click(function(){
+    $(".panel4").slideDown(500).delay(3500).slideUp(500);
+  }); 
+
+
+$(".affs").click(function(){
+    $(".panel5").slideDown(500).delay(3500).slideUp(500);
+ 
+  }); 
+  
+$(".taco").click(function(){
+    $(".panel6").slideDown(500).delay(3500).slideUp(500);
+  });   
+  
+$(".tkml").click(function(){
+    $(".panel7").slideDown(500).delay(3500).slideUp(500);
+	  }); 
+
+});
